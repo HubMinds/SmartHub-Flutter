a102dd7a369019c23b5576129ee546ad2e720cdf
Zak Taylor <zak.txyor@gmail.com>
2024-04-25 17:30:06 +0100

    added a sign out button to the left of smarthub on home screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print, use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -58,7 +60,7 @@ class LoginScreenState extends State<LoginScreen> {
     await FirebaseAuth.instance.signOut();
     Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => RegisterScreen()),
+              MaterialPageRoute(builder: (context) => const RegisterScreen()),
             );
     print("User signed out");
   } catch (e) {
